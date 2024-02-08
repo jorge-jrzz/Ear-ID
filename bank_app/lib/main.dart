2fc0619e33b995a32265dce2e8a5c1fe82280fc3
ivangv0505 <ivan_garrido51@outlook.com>
2024-02-08 15:05:20 -0600

    Prototipo de una app bancaria con el reconocimiento auricular

--- bank_app/lib/main.dart
+++ bank_app/lib/main.dart
@@ -1,125 +1,227 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<List<CameraDescription>> getCameras() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  return await availableCameras();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() => runApp(const BEBA());
 
-  // This widget is the root of your application.
+class BEBA extends StatelessWidget {
+  const BEBA({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomePageState extends State<HomePage> {
+  List<CameraDescription>? cameras;
+  CameraController? controller;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+            child: Text("BEBA",
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ))),
+        backgroundColor: const Color.fromARGB(255, 4, 36, 83),
+      ),
+      backgroundColor: Colors.blueAccent,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('EL BANCO DE MÉXICO',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ))
+              ],
+            ),
+            Image.asset('assets/imagexd.gif'),
+            Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () async {
+                    final cameras = await getCameras();
+                    final controller = CameraController(
+                        cameras[0], ResolutionPreset.medium,
+                        enableAudio: false);
+                    await controller.initialize();
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) =>
+                            CameraPreviewScreen(controller: controller),
+                      ),
+                    );
+                  },
+                  child: Text('Iniciar'),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue.shade400,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+                      minimumSize: Size(double.infinity, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      )),
+                ),
+                ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Emergencias'),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue.shade400,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+                        minimumSize: Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ))),
+              ],
+            ),
+            if (controller != null && controller!.value.isInitialized)
+              AspectRatio(
+                aspectRatio: controller!.value.aspectRatio,
+                child: CameraPreview(controller!),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CameraPreviewScreen extends StatefulWidget {
+  final CameraController controller;
+
+  const CameraPreviewScreen({Key? key, required this.controller})
+      : super(key: key);
+
+  @override
+  _CameraPreviewScreenState createState() => _CameraPreviewScreenState();
+}
+
+class _CameraPreviewScreenState extends State<CameraPreviewScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse: true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+            child: Text("BEBA",
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ))),
+        backgroundColor: const Color.fromARGB(255, 4, 36, 83),
+      ),
+      backgroundColor: Colors.blueAccent,
+      body: Stack(
+        children: [
+          Center(
+            child: Container(
+              color: Colors.blue,
+              child: AspectRatio(
+                aspectRatio: widget.controller.value.aspectRatio,
+                child: CameraPreview(widget.controller),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 50,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: FadeTransition(
+                opacity: _controller,
+                child: Image.asset('assets/EarIDicon.png',
+                    width:
+                        100), // Asegúrate de reemplazar esto con la ruta a tu imagen
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
+class CameraScreen extends StatefulWidget {
+  final List<CameraDescription> cameras;
+
+  CameraScreen({required this.cameras});
+
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  late CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(widget.cameras[0], ResolutionPreset.medium,
+        enableAudio: false);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
     });
   }
 
   @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return CameraPreviewScreen(controller: controller);
   }
 }
